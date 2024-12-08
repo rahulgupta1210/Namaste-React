28781b130371aa89965a17c50b4e19e6c63e2457
rahulgupta1210 <pgupta.rahul88@gmail.com>
2024-12-09 00:10:48 +0530

    Parcel command added and executed
    Code changes parcel related

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
+import React from "react";
+//import ReactDOM  from "react-dom";
+import ReactDOM from "react-dom/client"
 
-{/* <div id="parent">
+/* <div id="parent">
     <div id="child">
         <h1></h1>
         <h2></h2>
     </div>
-</div> */}
+</div> */
 
 // const parent = React.createElement(
 //     'div',
--- read.md
+++ read.md
@@ -2,3 +2,6 @@
 //what is crossorigin in cdn
 //react dom is bridge between react cre and browser
 //it can work independent in small portion of app -library
+
+
+//ggit init
