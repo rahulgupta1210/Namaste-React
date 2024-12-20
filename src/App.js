2d3b3174b69a20a180c7abd7532f17884d95898e
rahulgupta1210 <pgupta.rahul88@gmail.com>
2024-12-21 00:37:48 +0530

    React fiber and useState

--- src/App.js
+++ src/App.js
@@ -5,18 +5,9 @@ import ReactDOM from "react-dom/client";
 import HeaderComponent from "./components/Header";
 import Body from "./components/Body";
 
-
-
-
-
-
-
-
-
-
-
-
 const Applayout = () => {
+console.log('Virtual DOOM e.g',<Body/>)//normal js obj
+
     return <div className="app">
         <HeaderComponent />
         <Body />
--- src/components/Body.js
+++ src/components/Body.js
@@ -1,46 +1,52 @@
 import RestaurantCard from "./RestaurantCard";
 import resList from "../../utils/mockData";
+
+import { useState } from "react";
 const Body = () => {
 
-    let listOfRestaurants = [
-        {
-            "info": {
-                "id": "41102",
-                "name": "Hotel Empire",
-                "cloudinaryImageId": "10f8b483521b9359d9c8fcd37f23c9d3",
-                "locality": "Majestic",
-                "areaName": "Majestic",
-                "costForTwo": "₹450 for two",
-                "cuisines": [
-                    "Kebabs",
-                    "Biryani"
-                ],
-                "avgRating": 4.3,
-                "parentId": "475",
-                "avgRatingString": "4.3",
-                "totalRatingsString": "14K+",
-                "promoted": true,
-            }
-        }, {
-            "info": {
-                "id": "41103",
-                "name": "Hotel New",
-                "cloudinaryImageId": "10f8b483521b9359d9c8fcd37f23c9d3",
-                "locality": "Majestic",
-                "areaName": "Majestic",
-                "costForTwo": "₹450 for two",
-                "cuisines": [
-                    "Kebabs",
-                    "Biryani"
-                ],
-                "avgRating": 3.5,
-                "parentId": "475",
-                "avgRatingString": "4.3",
-                "totalRatingsString": "14K+",
-                "promoted": true,
-            }
-        }
-    ];
+    //local state variable 
+    // let listOfRestaurants2 = [
+    //     {
+    //         "info": {
+    //             "id": "41102",
+    //             "name": "Hotel Empire",
+    //             "cloudinaryImageId": "10f8b483521b9359d9c8fcd37f23c9d3",
+    //             "locality": "Majestic",
+    //             "areaName": "Majestic",
+    //             "costForTwo": "₹450 for two",
+    //             "cuisines": [
+    //                 "Kebabs",
+    //                 "Biryani"
+    //             ],
+    //             "avgRating": 4.3,
+    //             "parentId": "475",
+    //             "avgRatingString": "4.3",
+    //             "totalRatingsString": "14K+",
+    //             "promoted": true,
+    //         }
+    //     }, {
+    //         "info": {
+    //             "id": "41103",
+    //             "name": "Hotel New",
+    //             "cloudinaryImageId": "10f8b483521b9359d9c8fcd37f23c9d3",
+    //             "locality": "Majestic",
+    //             "areaName": "Majestic",
+    //             "costForTwo": "₹450 for two",
+    //             "cuisines": [
+    //                 "Kebabs",
+    //                 "Biryani"
+    //             ],
+    //             "avgRating": 3.5,
+    //             "parentId": "475",
+    //             "avgRatingString": "4.3",
+    //             "totalRatingsString": "14K+",
+    //             "promoted": true,
+    //         }
+    //     }
+    // ];
+    const [listOfRestaurants,setListOfRestaurants] = useState( resList);//whatever we pass will be the initial value
+
+    
 
     return (
         <div className="body">
@@ -48,9 +54,11 @@ const Body = () => {
                 <button className="filter-button" 
                 onClick={() => {
                     console.log('clicked');
-                    listOfRestaurants = listOfRestaurants.filter(
+
+                    const filteredListOfRestaurants = listOfRestaurants.filter(
                         (item) => item.info.avgRating > 4
                     )
+                    setListOfRestaurants(filteredListOfRestaurants);
                     console.log('listOfRestaurants',listOfRestaurants)
                 }}
                 >
