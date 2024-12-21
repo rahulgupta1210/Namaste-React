79429e0c2ef3edde4838a5fe1f12999a4b6d8423
rahulgupta1210 <pgupta.rahul88@gmail.com>
2024-12-22 00:36:16 +0530

    added swiggy api

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,7 +2,31 @@ import RestaurantCard from "./RestaurantCard";
 import resList from "../../utils/mockData";
 
 import { useState } from "react";
+import { useEffect } from "react";
 const Body = () => {
+
+    useEffect(() => {
+        console.log('useeffect will render after body render');
+        fetchData();
+    }, [])
+
+
+    const fetchData = async ()=>{
+        const data = await fetch('https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.902157887494267&lng=77.70266828616035&collection=83637&tags=layout_CCS_Burger&sortBy=&filters=&type=rcv2&offset=0&page_type=null');
+        const json = await data.json();
+        console.log(json?.data?.cards[2]?.data?.data?.cards);
+
+        //setListOfRestaurants(json?.data?.cards[2]?.data?.data?.cards)
+    }
+
+   
+    
+
+
+    console.log('body rendered')
+
+
+    //once body component render then useEffect hook will be called after component has been rendered.
 
     //local state variable 
     // let listOfRestaurants2 = [
@@ -44,23 +68,23 @@ const Body = () => {
     //         }
     //     }
     // ];
-    const [listOfRestaurants,setListOfRestaurants] = useState( resList);//whatever we pass will be the initial value
+    const [listOfRestaurants, setListOfRestaurants] = useState(resList);//whatever we pass will be the initial value
 
-    
+
 
     return (
         <div className="body">
             <div className="filter">
-                <button className="filter-button" 
-                onClick={() => {
-                    console.log('clicked');
+                <button className="filter-button"
+                    onClick={() => {
+                        console.log('clicked');
 
-                    const filteredListOfRestaurants = listOfRestaurants.filter(
-                        (item) => item.info.avgRating > 4
-                    )
-                    setListOfRestaurants(filteredListOfRestaurants);
-                    console.log('listOfRestaurants',listOfRestaurants)
-                }}
+                        const filteredListOfRestaurants = listOfRestaurants.filter(
+                            (item) => item.info.avgRating > 4
+                        )
+                        setListOfRestaurants(filteredListOfRestaurants);
+                        console.log('listOfRestaurants', listOfRestaurants)
+                    }}
                 >
                     Top Rated Button</button>
             </div>
